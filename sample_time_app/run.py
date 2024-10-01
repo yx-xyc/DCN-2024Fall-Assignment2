da45280cc55003962c01db442e6db666fd11b503
yx-xyc <yanchong@mac.myfiosgateway.com>
2024-09-30 20:24:53 -0400

    add /time endpoint

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+# time endpoint to return current time
+@app.route('/time')
+def get_time():
+    current_time = str(datetime.now())
+    return current_time
 
 
 app.run(host='0.0.0.0',
